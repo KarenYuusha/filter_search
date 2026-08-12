db1f761949afa67a2bf2db6283736ba87b7c563d
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-12 11:55:39 +0700

    feat: export canonical skill parsing

--- toram_skills/__init__.py
+++ toram_skills/__init__.py
@@ -1,7 +1,19 @@
+from .models import ImportReport, ParseIssue, ParsedSkillFile, SkillDraft, SkillSection, SkillTreeDraft
+from .parsing import normalize_skill_name, parse_standard_skill_file, skill_id, skill_tree_id
 from .source_inventory import SkillSource, discover_skill_sources, source_manifest_hash
 
 __all__ = [
+    "ImportReport",
+    "ParseIssue",
+    "ParsedSkillFile",
+    "SkillDraft",
+    "SkillSection",
     "SkillSource",
+    "SkillTreeDraft",
     "discover_skill_sources",
+    "normalize_skill_name",
+    "parse_standard_skill_file",
+    "skill_id",
+    "skill_tree_id",
     "source_manifest_hash",
 ]
