609c0ed6dfdd389284bc001b37fcf4daf8301652
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-12 20:51:46 +0700

    feat: export embedding provider adapters

--- toram_skill_embeddings/__init__.py
+++ toram_skill_embeddings/__init__.py
@@ -1,3 +1,11 @@
 from .ollama_provider import OllamaEmbeddingProvider
+from .providers import EmbeddingProviderSpec, build_provider, parse_provider_spec
+from .sentence_transformer_provider import SentenceTransformerEmbeddingProvider
 
-__all__ = ["OllamaEmbeddingProvider"]
+__all__ = [
+    "EmbeddingProviderSpec",
+    "OllamaEmbeddingProvider",
+    "SentenceTransformerEmbeddingProvider",
+    "build_provider",
+    "parse_provider_spec",
+]
