a7ce35a0d787d93271cf6d45ecef339081fa2208
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-12 16:50:53 +0700

    feat: export skill search models

--- toram_skills/__init__.py
+++ toram_skills/__init__.py
@@ -3,16 +3,21 @@ from .models import ImportReport, ParseIssue, ParsedSkillFile, SkillDraft, Skill
 from .parsing import normalize_skill_name, parse_skill_file, parse_standard_skill_file, skill_id, skill_tree_id
 from .repository import SchemaError, SkillRepository
 from .schema import REQUIRED_TABLES, create_schema, verify_schema
+from .search_models import ChannelScore, SearchChannel, SkillFilters, SkillSearchHit
 from .source_inventory import SkillSource, discover_skill_sources, source_manifest_hash
 
 __all__ = [
+    "ChannelScore",
     "ImportReport",
     "ParseIssue",
     "ParsedSkillFile",
     "REQUIRED_TABLES",
     "SchemaError",
+    "SearchChannel",
     "SkillDraft",
+    "SkillFilters",
     "SkillRepository",
+    "SkillSearchHit",
     "SkillSection",
     "SkillSource",
     "SkillTreeDraft",
