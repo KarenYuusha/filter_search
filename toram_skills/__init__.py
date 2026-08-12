8c0bce3778187b37464d1b466dab880507223973
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-12 15:59:14 +0700

    feat: export canonical skill database interfaces

--- toram_skills/__init__.py
+++ toram_skills/__init__.py
@@ -1,19 +1,29 @@
+from .importer import import_skill_corpus
 from .models import ImportReport, ParseIssue, ParsedSkillFile, SkillDraft, SkillSection, SkillTreeDraft
-from .parsing import normalize_skill_name, parse_standard_skill_file, skill_id, skill_tree_id
+from .parsing import normalize_skill_name, parse_skill_file, parse_standard_skill_file, skill_id, skill_tree_id
+from .repository import SchemaError, SkillRepository
+from .schema import REQUIRED_TABLES, create_schema, verify_schema
 from .source_inventory import SkillSource, discover_skill_sources, source_manifest_hash
 
 __all__ = [
     "ImportReport",
     "ParseIssue",
     "ParsedSkillFile",
+    "REQUIRED_TABLES",
+    "SchemaError",
     "SkillDraft",
+    "SkillRepository",
     "SkillSection",
     "SkillSource",
     "SkillTreeDraft",
+    "create_schema",
     "discover_skill_sources",
+    "import_skill_corpus",
     "normalize_skill_name",
+    "parse_skill_file",
     "parse_standard_skill_file",
     "skill_id",
     "skill_tree_id",
     "source_manifest_hash",
+    "verify_schema",
 ]
