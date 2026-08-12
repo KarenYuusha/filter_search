55315391df30d435915d7b11c5c7b350a6b66b45
Khoa <62637453+KarenYuusha@users.noreply.github.com>
2026-08-12 11:51:50 +0700

    feat: add skill package exports

---
+++ toram_skills/__init__.py
@@ -0,0 +1,7 @@
+from .source_inventory import SkillSource, discover_skill_sources, source_manifest_hash
+
+__all__ = [
+    "SkillSource",
+    "discover_skill_sources",
+    "source_manifest_hash",
+]
